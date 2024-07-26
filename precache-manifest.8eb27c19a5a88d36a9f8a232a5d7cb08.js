fdccf127572e8144330142747d5b43b62ffce9e7
Nirosha <nishipalpala@gmail.com>
2024-07-26 13:07:15 +0530

    Updates

---
+++ precache-manifest.8eb27c19a5a88d36a9f8a232a5d7cb08.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "08193347cd1bb0b737a4691eae7f3de5",
+    "url": "/collaboratehub_frontend/index.html"
+  },
+  {
+    "revision": "ff1a150bb7bb8d4a69b5",
+    "url": "/collaboratehub_frontend/static/css/2.12f8b395.chunk.css"
+  },
+  {
+    "revision": "58f37c316edf5bdc59c4",
+    "url": "/collaboratehub_frontend/static/css/main.cfad6413.chunk.css"
+  },
+  {
+    "revision": "ff1a150bb7bb8d4a69b5",
+    "url": "/collaboratehub_frontend/static/js/2.530cd6ce.chunk.js"
+  },
+  {
+    "revision": "58f37c316edf5bdc59c4",
+    "url": "/collaboratehub_frontend/static/js/main.e431278a.chunk.js"
+  },
+  {
+    "revision": "cd6d54967e9ff67b0682",
+    "url": "/collaboratehub_frontend/static/js/runtime~main.5d41f9e6.js"
+  }
+]);
